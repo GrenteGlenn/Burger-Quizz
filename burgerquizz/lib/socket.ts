088dfeb7d44063e14510207cfc3b27e31f4055e3
GrenteGlenn <grenteglenn@gmail.com>
2026-05-29 01:43:54 +0200

    [FIX]Mise en prod

--- burgerquizz/lib/socket.ts
+++ burgerquizz/lib/socket.ts
@@ -1,5 +1,12 @@
+// import { io } from "socket.io-client";
+
+// export const socket = io("http://localhost:3000", {
+//   transports: ["websocket"],
+// });
+
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
   transports: ["websocket"],
+  reconnection: true,
 });
